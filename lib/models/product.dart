790d651eed527c43c58cc335db900c385a527c21
Marta Reinhardt <martinhareinhardt@gmail.com>
2026-05-22 15:07:57 -0300

    Create product.dart

---
+++ lib/models/product.dart
@@ -0,0 +1,55 @@
+class Product {
+  final int? id;
+  final int userId;
+  final String name;
+  final int quantity;
+  final double price;
+  final DateTime expiryDate;
+  final String condition;
+  final bool isProducer;
+  final String address;
+  final double wastePreventedKg;
+
+  Product({
+    this.id,
+    required this.userId,
+    required this.name,
+    required this.quantity,
+    required this.price,
+    required this.expiryDate,
+    required this.condition,
+    required this.isProducer,
+    required this.address,
+    this.wastePreventedKg = 0.0,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'user_id': userId,
+      'name': name,
+      'qty': quantity,
+      'price': price,
+      'expiry_date': expiryDate.toIso8601String(),
+      'condition': condition,
+      'is_producer': isProducer ? 1 : 0,
+      'address': address,
+      'waste_prevented_kg': wastePreventedKg,
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      id: map['id'],
+      userId: map['user_id'],
+      name: map['name'],
+      quantity: map['qty'],
+      price: map['price'],
+      expiryDate: DateTime.parse(map['expiry_date']),
+      condition: map['condition'],
+      isProducer: map['is_producer'] == 1,
+      address: map['address'],
+      wastePreventedKg: map['waste_prevented_kg'] ?? 0.0,
+    );
+  }
+}
