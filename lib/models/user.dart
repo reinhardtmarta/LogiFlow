ed3af499488cc453fa12a2f78e01e6dc280e55ec
Marta Reinhardt <martinhareinhardt@gmail.com>
2026-08-07 07:23:07 -0300

    Refactor User model for Firestore integration
    Removed password field and updated methods for Firestore compatibility.
    
    Signed-off-by: Marta Reinhardt  <martinhareinhardt@gmail.com>

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,7 +2,6 @@ class User {
   final String? id;
   final String name;
   final String email;
-  final String password;
   final String phone;
   final String address;
   final bool isSeller;
@@ -11,33 +10,29 @@ class User {
     this.id,
     required this.name,
     required this.email,
-    required this.password,
     required this.phone,
     required this.address,
     required this.isSeller,
   });
 
-  Map<String, dynamic> toMap() {
+  Map<String, dynamic> toFirestore() {
     return {
-      'id': id,
       'name': name,
       'email': email,
-      'password': password,
       'phone': phone,
       'address': address,
-      'is_seller': isSeller ? 1 : 0,
+      'isSeller': isSeller,
     };
   }
 
-  factory User.fromMap(Map<String, dynamic> map) {
+  factory User.fromFirestore(Map<String, dynamic> map) {
     return User(
       id: map['id']?.toString(),
       name: map['name'] ?? '',
       email: map['email'] ?? '',
-      password: map['password'] ?? '',
       phone: map['phone'] ?? '',
       address: map['address'] ?? '',
-      isSeller: (map['is_seller'] == 1) || (map['is_seller'] == true),
+      isSeller: map['isSeller'] ?? false,
     );
   }
 }
